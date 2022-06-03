ba86fc14b25eba4599440f7b0af748c76c20ffbc
bl4ckck <alvin.naufal3@gmail.com>
2022-06-03 23:44:08 +0700

    feat: setup nx (#2)
    * feat: setup nx for react and fastify
    
    * fix: changed path github workflows
    
    * chore: github workflows on pull request into main
    
    * fix: yarn ci to npm ci
    
    * fix: change run ci into yarn frozen lockfile
    
    * fix: github ci run into yarn immutable cache
    
    * fix: nx affected base into HEAD~1

---
+++ apps/api/src/main.ts
@@ -0,0 +1,11 @@
+import { build } from "./app";
+
+const server = build();
+const port = process.env.PORT || 3000;
+
+server.listen(port, function (err, address) {
+  if (err) {
+    server.log.error(err);
+    process.exit(1);
+  };
+});
